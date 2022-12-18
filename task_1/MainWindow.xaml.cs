6c34ed4d7d7aa37f0eae7b2e8ccba4998417f762
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-12-18 14:49:05 +0200

    Add project files.

---
+++ task_1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    /// <summary>
+    ///                 Тема «Контейнеры компоновки»
+    ///                         Задание 1
+    /// Разработать WPF-приложение, содержащее набор кнопок, занимающих
+    /// 2/3 ширины окна при любых его размерах. Каждая кнопка должна в качестве
+    /// содержимого отображать название цвета и обладать наружным отступом,
+    /// равным 2.0. Также соответствующий цвет должен использоваться в качестве
+    /// цвета переднего плана кнопки. Необходимо использовать следующий набор
+    /// цветов: Navy, Blue, Aqua, Teal, Olive, Green, Lime, Yellow, Orange, Red, Maroon,
+    /// Fuchsia, Purple, Black, Silver, Gray, White.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ task_2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    /// <summary>
+    ///                             Задание 2
+    /// Разработать WPF-приложение для ввода шифра. У правой границы окна
+    /// должна быть расположена цифровая панель. При изменении размеров окна
+    /// поле ввода цифр должно занимать все свободное пространство окна.
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
