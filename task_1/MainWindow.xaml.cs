9929c97f01380f1feb3c7ccd61b542f5af26fd10
blazhkevych.v <blazhkevych.v@gmail.com>
2023-01-03 00:09:34 +0200

    1, 2 ready

--- task_1/MainWindow.xaml.cs
+++ task_1/MainWindow.xaml.cs
@@ -1,38 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace task_1
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+namespace task_1;
 
-    /// <summary>
-    ///                 Тема «Контейнеры компоновки»                                                                                  /// Layout Containers topic
-    ///                         Задание 1                                                                                             ///                         Exercise 1
-    /// Разработать WPF-приложение, содержащее набор кнопок, занимающих 2/3 ширины окна при любых его размерах.                       /// Develop a WPF application containing a set of buttons that take up 2/3 of the width of the window at any size.
-    /// Каждая кнопка должна в качестве содержимого отображать название цвета и обладать наружным отступом, равным 2.0.               /// Each button should display the name of the color as content and have an outer padding of 2.0.
-    /// Также соответствующий цвет должен использоваться в качестве цвета переднего плана кнопки.                                     /// Also the corresponding color should be used as the foreground color of the button.
-    /// Необходимо использовать следующий набор цветов: Navy, Blue, Aqua, Teal, Olive, Green, Lime, Yellow, Orange, Red, Maroon,      /// The following color set must be used: Navy, Blue, Aqua, Teal, Olive, Green, Lime, Yellow, Orange, Red, Maroon,       
-    /// Fuchsia, Purple, Black, Silver, Gray, White.                                                                                  /// Fuchsia, Purple, Black, Silver, Gray, White.
-    /// </summary>
-    public partial class MainWindow : Window
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+/// <summary>
+/// Тема «Контейнеры компоновки»
+/// Задание 1
+/// Разработать WPF-приложение, содержащее набор кнопок, занимающих 2/3 ширины окна при любых его размерах.
+/// Каждая кнопка должна в качестве содержимого отображать название цвета и обладать наружным отступом, равным 2.0.
+/// Также соответствующий цвет должен использоваться в качестве цвета переднего плана кнопки.
+/// Необходимо использовать следующий набор цветов: Navy, Blue, Aqua, Teal, Olive, Green, Lime, Yellow, Orange, Red, Maroon,
+/// Fuchsia, Purple, Black, Silver, Gray, White.
+///
+/// Layout Containers topic
+/// Exercise 1
+/// Develop a WPF application containing a set of buttons that take up 2/3 of the width of the window at any size.
+/// Each button should display the name of the color as content and have an outer padding of 2.0.
+/// Also the corresponding color should be used as the foreground color of the button.
+/// The following color set must be used: Navy, Blue, Aqua, Teal, Olive, Green, Lime, Yellow, Orange, Red, Maroon,
+/// Fuchsia, Purple, Black, Silver, Gray, White.
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
--- task_2/MainWindow.xaml.cs
+++ task_2/MainWindow.xaml.cs
@@ -1,53 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace task_2
+namespace task_2;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+/// <summary>
+///     Task 2
+///     Develop a WPF application for entering a ciphers.
+///     A number pad should be located at the right border of the window.
+///     When resizing the window, the number input field must occupy all the free space of the window.
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    // Variable for storing a password.
+    private readonly string _password = "12345";
 
-    /// <summary>
-    ///                             Задание 2                                                              /// Task 2
-    /// Разработать WPF-приложение для ввода шифра.                                                        /// Develop a WPF application for entering a cipher.
-    /// У правой границы окна должна быть расположена цифровая панель.                                     /// A number pad should be located at the right border of the window.
-    /// При изменении размеров окна поле ввода цифр должно занимать все свободное пространство окна.       /// When resizing the window, the number input field must occupy all the free space of the window.
-    /// </summary>
-
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void Button7ClickEvent(object sender, RoutedEventArgs e)
-        {
-            // 2:56:30 about second task
-            // Добавление цифры в поле ввода из кнопок, где значение content от 0 до 9.
-            // Adding a digit to the input field from the buttons, where the value content is from 0 to 9.
-            if (sender is Button button)
-            {
-                if (button.Content is string content)
-                {
-                    if (content[0] >= '0' && content[0] <= '9')
-                    {
-                        TextBox1.Text += content;
-                    }
-                }
-            }
-        }
+    // Handler for the event of pressing the number pad button.
+    private void ButtonNumbersClickEvent(object sender, RoutedEventArgs e)
+    {
+        // Adding a digit to the input field from the buttons, where the value content is from 0 to 9.
+        if (sender is Button button)
+            if (button.Content is string content)
+                if (content[0] >= '0' && content[0] <= '9')
+                    TextBox1.Text += content;
+    }
+
+    // Handler for the event of pressing the "Clear" button.
+    private void ButtonC_OnClickClickEvent(object sender, RoutedEventArgs e)
+    {
+        TextBox1.Text = "";
+    }
+
+    // Handler for the event of pressing the "OK" button.
+    private void ButtonOk_OnClickClickEvent(object sender, RoutedEventArgs e)
+    {
+        if (TextBox1.Text == _password)
+            MessageBox.Show("Password correct", "Password", MessageBoxButton.OK, MessageBoxImage.Information);
+        else
+            MessageBox.Show("Wrong password", "Password", MessageBoxButton.OK, MessageBoxImage.Error);
     }
 }
